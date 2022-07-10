c624a011e1a7d7fc12c99964468356e9f0ea76f5
billyboy35 <kevin.niglaut@wanadoo.fr>
2022-07-10 16:57:40 +0200

    billyboy35/WebErpMesv2/issues/63 & billyboy35/WebErpMesv2/issues/62

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,47 +10,47 @@
 
 @section('content')
     <div class="card">
-      <div class="table-responsive">
-        <table class="table">
-            <thead>
-                <tr>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Code</th>
+                        <th>Label</th>
+                        <th>Created At</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @php $Table =''  @endphp
+                    @forelse ($results as $result)
+                    @if( $result->getTable() != $Table) 
+                    <tr>
+                    <th colspan="4" class="bg-secondary disabled color-palette">{{ $result->getTable() }}</th>
+                    @php $Table = $result->getTable()  @endphp
+                    </tr>
+                    @endif
+                    <tr>
+                        <td>{{ $result->code }}</td>
+                        <td>{{ $result->label }}</td>
+                        <td>{{ $result->GetPrettyCreatedAttribute() }}</td>
+                        <td>
+                            <x-ButtonTextView route="{{ route( $result->getTable() .'.show', ['id' => $result->id])}}" />
+                        </td>
+                    </tr>
+                    @empty
+                        <x-EmptyDataLine col="4" text="No lines found ..."  />
+                    @endforelse
+                </tbody>
+                <tfoot>
+                    <tr>
                     <th>Code</th>
                     <th>Label</th>
                     <th>Created At</th>
                     <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-              @php $Table =''  @endphp
-                @forelse ($results as $result)
-                @if( $result->getTable() != $Table) 
-                <tr>
-                  <th colspan="4" class="bg-secondary disabled color-palette">{{ $result->getTable() }}</th>
-                  @php $Table = $result->getTable()  @endphp
-                </tr>
-                @endif
-                <tr>
-                    <td>{{ $result->code }}</td>
-                    <td>{{ $result->label }}</td>
-                    <td>{{ $result->GetPrettyCreatedAttribute() }}</td>
-                    <td>
-                        <x-ButtonTextView route="{{ route( $result->getTable() .'.show', ['id' => $result->id])}}" />
-                    </td>
-                </tr>
-                @empty
-                    <x-EmptyDataLine col="4" text="No lines found ..."  />
-                @endforelse
-            </tbody>
-            <tfoot>
-                <tr>
-                  <th>Code</th>
-                  <th>Label</th>
-                  <th>Created At</th>
-                  <th>Action</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
     </div>
     <!-- /.card -->
 @stop
