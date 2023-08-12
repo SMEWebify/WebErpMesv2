6dc65d723890b83393661d90df7011623507e6ab
billyboy35 <kevin.niglaut@wanadoo.fr>
2023-08-12 23:25:49 +0200

    Add SMEWebify/WebErpMesv2/issues/187

---
+++ resources/views/components/button-text-delete.blade.php
@@ -0,0 +1,4 @@
+<a class="btn btn-danger btn-sm" href="{{ $route }}">
+    <i class="fas fa-trash"></i>
+    Delete
+</a>
---
+++ resources/views/components/button-text-edit.blade.php
@@ -0,0 +1,4 @@
+<a class="btn btn-info btn-sm" href="{{ $route }}">
+    <i class="fas fa-pencil-alt"></i>
+    Edit
+</a>
