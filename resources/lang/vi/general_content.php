d66f9f95706abe715bf8ad404f5aa5eab65292c9
Dzung Do <dungdo@osd.vn>
2023-09-15 02:40:24 +0700

    Update general_content.php

--- resources/lang/vi/general_content.php
+++ resources/lang/vi/general_content.php
@@ -1,1 +1,119 @@
-vi/
+<?php
+
+return [
+    //STATUS
+    'open_trans_key'                           => 'Mở',
+    'started_trans_key'                        => 'Đã bắt đầu',
+    'in_progress_trans_key'                    => 'Tiến trình',
+    'finished_trans_key'                       => 'Đã kết thúc',
+    'delivered_trans_key'                      => 'Đã giao hàng',
+    'partly_delivered_trans_key'               => 'Partly delivered',
+
+    //GENERAL HEADER TABLE
+    'id_trans_key'                             => 'ID',
+    'code_trans_key'                           => 'Project Ref',
+    'label_trans_key'                          => 'Nhãn',
+    'customer_trans_key'                       => 'Khách hàng',
+    'status_trans_key'                         => 'Trạng thái',
+    'total_price_trans_key'                    => 'Tổng giá',
+    'created_at_trans_key'                     => 'Created at',
+    'assigned_trans_key'                       => 'Assigned',
+    'action_trans_key'                         => 'Actions',
+
+    // BUTTON
+    'view_all_trans_key'                       => 'Xem toàn bộ',
+
+    //DASHBOARD
+    'dashboard_trans_key'                      => 'Dashboard',
+    'client_trans_key'                         => 'Khách hàng',
+    'suppliers_trans_key'                      => 'Nhà cung cấp',
+    'view_details_trans_key'                   => 'Xem chi tiết',
+    'monthly_recap_report_trans_key'           => 'Monthly Recap Report',
+    'monthly_recap_task_trans_key'             => 'Monthly Recap Task',
+    'sales_period_trans_key'                   => 'Sales: 1 Jan, :YEAR - 30 Dec, :YEAR',
+    'incoming_orders_trans_key'                => 'Incoming orders',
+    'no_coming_orders_trans_key'               => 'No coming orders.',
+    'late_orders_trans_key'                    => 'Late orders',
+    'no_late_orders_trans_key'                 => 'No late orders.',
+    'goal_task_trans_key'                      => 'Goal Task Completion',
+    'no_task_trans_key'                        => 'No service go to Methods for add.',
+    'total_order_forcasted_trans_key'          => 'TOTAL ORDER FORCASTED',
+    'total_order_delivered_trans_key'          => 'TOTAL ORDER DELIVERED',
+    'total_invoiced_trans_key'                 => 'TOTAL INVOICED',
+    'goal_trans_key'                           => 'GOAL COMPLETIONS',
+    'quote_transformation_trans_key'           => 'Quote transformation rate',
+    'latest_quotes_trans_key'                  => 'Latest Quotes',
+    'no_quote_trans_key'                       => 'No quote, go quote page for add quote',
+    'latest_orders_trans_key'                  => 'Latest Orders',
+    'no_order_trans_key'                       => 'No order, go order page for add order',
+    'latest_products_trans_key'                => 'Recently Added Products',
+    'no_product_trans_key'                     => 'No product, go product page for add item',
+    'announcement_trans_key'                   => 'Announcements',
+    'order_to_be_delivered_trans_key'          => 'Order to be delivered',
+    'delivered_month_in_progress_trans_key'    => 'Total Delivered for the month',
+    'remaining_month_trans_key'                => 'Total remaining to deliver',
+    
+
+    'companies_trans_key'                      => 'Companies',
+
+    'leads_trans_key'                          => 'Leads',
+
+    'quote_trans_key'                          => 'Quote',
+    'quotes_list_trans_key'                    => 'quotes list',
+    'quotes_lines_list_trans_key'              => 'quotes lines list',
+    
+    'orders_trans_key'                         => 'Orders',
+    'orders_list_trans_key'                    => 'Orders list',
+    'orders_lines_list_trans_key'              => 'Orders lines list',
+    'internal_order_trans_key'                 => 'Internal order',
+    
+
+
+    'scheduling_trans_key'                     => 'Scheduling',
+    'order_calendar_trans_key'                 => 'Order calendar',
+    'tasks_trans_key'                          => 'Tâches',
+    'tasks_list_trans_key'                     => 'Tasks list',
+    'tasks_statu_trans_key'                    => 'Task management',
+    'workflow_trans_key'                       => 'Workflow',
+    'gantt_trans_key'                          => 'Gantt',
+    'delivery_notes_trans_key'                 => 'Delivery notes',
+    'deliverys_notes_request_trans_key'        => 'Deliverys notes request',
+    'deliverys_notes_list_trans_key'           => 'Deliverys notes list',
+    'invoices_trans_key'                       => 'Invoices',
+    'invoices_request_trans_key'               => 'Invoices  request',
+    'invoices_list_trans_key'                  => 'Invoices  list',
+    'others_trans_key'                         => 'Others',
+
+    //PRODUCT
+    'product_trans_key'                        => 'Product',
+    'purchased_price_trans_key'                => 'Purchased price',
+    'selling_price_trans_key'                  => 'Selling price ',
+    
+    'product_list_trans_key'                   => 'Product list',
+    'stock_trans_key'                          => 'Stock',
+    'inventory_trans_key'                      => 'Inventory',
+    'purchase_trans_key'                       => 'Purchase',
+    'purchase_request_trans_key'               => 'Purchase request',
+    'requests_for_quotation_list_trans_key'    => 'Requests for quotation list',
+    'purchase_list_trans_key'                  => 'Purchase list',
+    'waiting_to_receipt_trans_key'             => 'Waiting to receipt',
+    'po_receipt_trans_key'                     => 'PO receipt',
+    'waiting_to_invoice_trans_key'             => 'Waiting to invoice',
+    'invoice_supplier_trans_key'               => 'Invoice supplier',
+
+    //QUALITY
+    'quality_trans_key'                        => 'Chất lượng',
+    'non_conformities_trans_key'               => 'Non conformities',
+    
+
+    'settings_trans_key'                       => 'Thiết lập',
+    'methods_trans_key'                        => 'Phương thức',
+    'accouting_trans_key'                      => 'Kế toán',
+
+    'users_trans_key'                          => 'Người dùng',
+    'your_company_trans_key'                   => 'Công ty',
+    'licence_trans_key'                        => 'Giấy phép',
+    'release_note_trans_key'                   => 'Nhật ký phát hành',
+    
+    'search_results_trans_key'                 => 'Kết quả tìm kiếm',
+];
