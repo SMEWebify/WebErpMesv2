0237a51104f79680bf2e2ebdd69a4e5331ef0583
billyboy35 <kevin.niglaut@wanadoo.fr>
2024-04-01 22:11:04 +0200

    Releases notes 1.08

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -18,7 +18,7 @@ class UserTableSeeder extends Seeder
     public function run() {
         for ($i=1; $i < 50; $i++) {
             $user = User::create([
-                'name' => 'SMEWebify/WebErpMesv2/issues/184 '.$i,
+                'name' => 'User '.$i,
                 'email' => 'test'.$i.'@test.com',
                 'email_verified_at' => now(),
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
