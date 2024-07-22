91e2333a564b5473a3ee9bc582fd044f95aa3497
billyboy35 <kevin.niglaut@wanadoo.fr>
2024-07-22 21:30:29 +0200

    Add SMEWebify/WebErpMesv2/issues/497

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Providers\FactoryServiceProvider;
+
 return [
 
     /*
@@ -177,7 +179,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\FactoryServiceProvider::class,
+        FactoryServiceProvider::class,
     ],
 
     /*
