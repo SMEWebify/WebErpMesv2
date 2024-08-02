6a9554e63d238045377c67bb3869af25a494d140
billyboy35 <kevin.niglaut@wanadoo.fr>
2024-08-02 22:55:49 +0200

    Fixe SMEWebify/WebErpMesv2/issues/502

--- app/Http/Controllers/Workshop/Controller.php
+++ app/Http/Controllers/Workshop/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Workflow;
+namespace App\Http\Controllers\Workshop;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
