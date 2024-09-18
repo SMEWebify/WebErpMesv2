20b96df56ab7be8ecfd6ba211a7bed8f31ec81ec
billyboy35 <kevin.niglaut@wanadoo.fr>
2024-09-18 22:21:33 +0200

    Fixe SMEWebify/WebErpMesv2/issues/542

--- database/migrations/2024_08_10_211106_remove_companies_contacts_and_addresses_from_purchase_receipts_table.php
+++ database/migrations/2024_08_10_211106_remove_companies_contacts_and_addresses_from_purchase_receipts_table.php
@@ -14,10 +14,6 @@ return new class extends Migration
     public function up()
     {
         Schema::table('purchase_receipts', function (Blueprint $table) {
-            // Supprime les clés étrangères
-            $table->dropForeign(['companies_contacts_id']);
-            $table->dropForeign(['companies_addresses_id']);
-
             // Supprime les colonnes
             $table->dropColumn('companies_contacts_id');
             $table->dropColumn('companies_addresses_id');
@@ -35,10 +31,6 @@ return new class extends Migration
             // Réintroduit les colonnes
             $table->unsignedBigInteger('companies_contacts_id');
             $table->unsignedBigInteger('companies_addresses_id');
-
-            // Réintroduit les clés étrangères
-            $table->foreign('companies_contacts_id')->references('id')->on('companies_contacts')->onDelete('cascade');
-            $table->foreign('companies_addresses_id')->references('id')->on('companies_addresses')->onDelete('cascade');
-        });
+});
     }
 };
