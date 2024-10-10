be47807bda6cd7f1f57e654085ba3a19f6458bc4
billyboy35 <kevin.niglaut@wanadoo.fr>
2024-10-10 22:41:03 +0200

    Story SMEWebify/WebErpMesv2/issues/136

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -108,38 +108,73 @@ class PrintController extends Controller
         $image = $Factory->getImageFactoryPath();
         $dompdf = PDF::loadView('print/pdf-invoice', compact('typeDocumentName', 'Document', 'Factory', 'image', 'totalPrices', 'subPrice', 'vatPrice', 'image'));
 
+        // Récupération des informations client depuis le modèle associé
+        $client = $Document->companie;
+        $clientAddress = $client->Addresses();
+
         $zugferddatas = ZugferdDocumentBuilder::CreateNew(ZugferdProfiles::PROFILE_EN16931);
         $zugferddatas
-            ->setDocumentInformation("471102", "380", \DateTime::createFromFormat("Ymd", "20180305"), "EUR")
-            ->addDocumentNote('Rechnung gemäß Bestellung vom 01.03.2018.')
-            ->addDocumentNote('Lieferant GmbH' . PHP_EOL . 'Lieferantenstraße 20' . PHP_EOL . '80333 München' . PHP_EOL . 'Deutschland' . PHP_EOL . 'Geschäftsführer: Hans Muster' . PHP_EOL . 'Handelsregisternummer: H A 123' . PHP_EOL . PHP_EOL, null, 'REG')
-            ->setDocumentSupplyChainEvent(\DateTime::createFromFormat('Ymd', '20180305'))
-            ->addDocumentPaymentMean(ZugferdPaymentMeans::UNTDID_4461_58, null, null, null, null, null, "DE12500105170648489890", null, null, null)
-            ->setDocumentSeller("Lieferant GmbH", "549910")
-            ->addDocumentSellerGlobalId("4000001123452", "0088")
-            ->addDocumentSellerTaxRegistration("FC", "201/113/40209")
-            ->addDocumentSellerTaxRegistration("VA", "DE123456789")
-            ->setDocumentSellerAddress("Lieferantenstraße 20", "", "", "80333", "München", "DE")
-            ->setDocumentSellerContact("Heinz Mükker", "Buchhaltung", "+49-111-2222222", "+49-111-3333333", "info@lieferant.de")
-            ->setDocumentBuyer("Kunden AG Mitte", "GE2020211")
-            ->setDocumentBuyerReference("34676-342323")
-            ->setDocumentBuyerAddress("Kundenstraße 15", "", "", "69876", "Frankfurt", "DE")
-            ->addDocumentTax("S", "VAT", 275.0, 19.25, 7.0)
-            ->addDocumentTax("S", "VAT", 198.0, 37.62, 19.0)
-            ->setDocumentSummation(529.87, 529.87, 473.00, 0.0, 0.0, 473.00, 56.87, null, 0.0)
-            ->addDocumentPaymentTerm("Zahlbar innerhalb 30 Tagen netto bis 04.04.2018, 3% Skonto innerhalb 10 Tagen bis 15.03.2018")
-            ->addNewPosition("1")
-            ->setDocumentPositionProductDetails("Trennblätter A4", "", "TB100A4", null, "0160", "4012345001235")
-            ->setDocumentPositionGrossPrice(9.9000)
-            ->setDocumentPositionNetPrice(9.9000)
-            ->setDocumentPositionQuantity(20, "H87")
-            ->addDocumentPositionTax('S', 'VAT', 19)
-            ->setDocumentPositionLineSummation(198.0);
+        ->setDocumentInformation($Document->code, "380", \DateTime::createFromFormat("Ymd", "20180305"), $Factory->curency)
+        ->addDocumentNote('Facture du ' . $Document->GetPrettyCreatedAttribute())
+        
+        // Ajout des informations du vendeur (Factory)
+        ->setDocumentSeller($Factory->name, $Factory->id)
+        ->addDocumentSellerTaxRegistration("VA", $Factory->vat_num)
+        ->setDocumentSellerAddress(
+            $Factory->address, 
+            $Factory->zipcode, 
+            $Factory->city, 
+            $Factory->country
+        )
+        ->setDocumentSellerContact(
+            $Factory->contact_name, 
+            'N/A', 
+            $Factory->phone_number, 
+            'N/A',
+            $Factory->mail
+        )
 
+        // Ajout des informations du client
+        ->setDocumentBuyer($client->label, $client->code)
+        //->addDocumentBuyerReference($Document->customer_reference)
+        ->addDocumentBuyerTaxRegistration("VA", $client->intra_community_vat)
+        ->setDocumentBuyerAddress(
+            $clientAddress->adress ?? 'N/A', 
+            'N/A',
+            $clientAddress->zipcode ?? '00000', 
+            $clientAddress->city ?? 'Unknown', 
+            $clientAddress->country ?? 'XX'
+        )
+
+        // Ajout des informations de paiement
+        ->addDocumentPaymentMean(ZugferdPaymentMeans::UNTDID_4461_58, null, null, null, null, null, $Factory->iban, null, null, null);
+
+        // Ajout des taxes et lignes de produits
+        foreach($vatPrice as $vat) {
+            $zugferddatas->addDocumentTax("S", "VAT", $subPrice, $vat[1], $vat[0]); // $vat[1] est le montant, $vat[0] est le taux
+        }
+        $totalVAT = array_sum(array_column($vatPrice, 1));
+        $zugferddatas->setDocumentSummation($totalPrices, $totalPrices, $subPrice, $totalVAT, 0.0, $subPrice, null, null, 0.0);
+        //$zugferddatas->addDocumentPaymentTerm($Document->payment_condition['label']);
+
+        // Ajout des lignes de facture à partir des `invoiceLines`
+        foreach ($Document->Lines as $key => $line) {
+            $zugferddatas->addNewPosition($key + 1)
+                ->setDocumentPositionProductDetails($line->OrderLine['label'], $line->OrderLine['code'], $line->OrderLine['code'])
+                ->setDocumentPositionGrossPrice($line->OrderLine->selling_price)
+                ->setDocumentPositionNetPrice($line->OrderLine->selling_price)
+                ->setDocumentPositionQuantity($line->qty, "H87")
+                ->addDocumentPositionTax('S', 'VAT', $line->OrderLine->VAT['rate'])
+                ->setDocumentPositionLineSummation($line->OrderLine->selling_price * $line->qty);
+        }
+
+        // Génération du document PDF
         $zugferdpdf = new ZugferdDocumentPdfBuilder($zugferddatas, $dompdf->stream());
+        $pdfFilePath = public_path('pdf/invoices/' . $typeDocumentName . '-' . $Document->id . '.pdf');
         $zugferdpdf->generateDocument();
-        $zugferdpdf->saveDocument(public_path('pdf\invoices') . '\\' . $typeDocumentName . '-' . $Document->id . '.pdf');
-        return response()->file(public_path('pdf\invoices') . '\\' . $typeDocumentName . '-' . $Document->id . '.pdf');
+        $zugferdpdf->saveDocument($pdfFilePath);
+
+        return response()->file($pdfFilePath);
     }
 
     /**
