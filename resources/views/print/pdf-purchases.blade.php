b5982dd0464556eb39eafbd928a0b15f70ba65c2
billyboy35 <kevin.niglaut@wanadoo.fr>
2024-10-10 22:07:50 +0200

    Refactor print SMEWebify/WebErpMesv2/issues/553

--- resources/views/print/pdf-purchases.blade.php
+++ resources/views/print/pdf-purchases.blade.php
@@ -6,82 +6,101 @@
         <title>{{ $typeDocumentName}} - #{{ $Document->code }}</title>
         <style type="text/css">
             @page {
-                margin: 0px;
+                margin: 350px 25px 50px 25px; 
             }
             body {
-                margin: 0px;
+                margin: 0px; 
+                border: 1px solid {{ $Factory->pdf_header_font_color }};
             }
             * {
                 font-family: Verdana, Arial, sans-serif;
             }
             a {
-                color: #fff;
+                color: #000;
                 text-decoration: none;
             }
             table {
                 font-size: smaller;
+                border-collapse:collapse;
             }
             tfoot tr td {
                 font-weight: bold;
                 font-size: smaller;
             }
-            .invoice table {
-                margin: 15px;
-            }
-            .invoice h3 {
-                margin-left: 15px;
-            }
-            .information {
-                background-color: {{ $Factory->pdf_header_font_color }};
-                color: #FFF;
-            }
-            .information .logo {
-                margin: 5px;
-            }
-            .information table {
+            table th
+            {
                 padding: 10px;
+                margin-top:30px;
+            }
+            header { position: fixed; top: -350px; left: 0px; right: 0px; ; height: 350px; }
+            footer { position: fixed; bottom: -50px; left: 0px; right: 0px; background-color: {{ $Factory->pdf_header_font_color }}; height: 50px; }
+            footer .pagenum:before {
+                content: counter(page);
             }
         </style>
     </head>
     <body>
-        <div class="information">
-            <table width="100%">
+        <header>
+            <table width="100%; border: none; ">
+                <tr>
+                    <td align="left" style="width: 50%; background-color: {{ $Factory->pdf_header_font_color }}">
+                    </td>
+                    <td colspan="2" align="center" style="width: 400%; background-color: {{ $Factory->pdf_header_font_color }}">
+                        <h2>{{ $typeDocumentName }}</h2>
+                        <h3>{{ $Document->code }}</h3>
+                    </td>
+                </tr>
+                <tr>
                     <td align="left" style="width: 40%;">
                         <h3>{{ $Factory->name }}</h3>
                         <pre>
-                        {{ $Factory->address }}
-                        {{ $Factory->zipcode }} {{ $Factory->city }}
-                        {{ __('general_content.phone_trans_key') }} : {{ $Factory->phone_number }}
-                        {{ __('general_content.email_trans_key') }} : {{ $Factory->mail }}
-                        <br /><br />
-                {{ __('general_content.date_trans_key') }} : {{ date('Y-m-d') }}
+{{ $Factory->address }}
+{{ $Factory->zipcode }} {{ $Factory->city }}
+{{ __('general_content.phone_trans_key') }} : {{ $Factory->phone_number }}
+{{ __('general_content.email_trans_key') }} : {{ $Factory->mail }}
+<br />
+<br />
+{{ __('general_content.date_trans_key') }} : {{ date('Y-m-d') }}
                         </pre>
                     </td>
-                    <td align="center">
-                        @if($Factory->picture)
-                        <img src="data:image/png;base64,{{ $image }}" alt="Logo" width="64" class="logo"/>
+                    <td align="left" style="width: 50%;">
+                        @if($Document->type == 1 || empty($Document->type))
+                            <h3>{{ $Document->companie['label'] }} </h3>
+                            <pre>
+{{ $Document->contact['civility'] }} {{ $Document->contact['first_name'] }} {{ $Document->contact['name'] }}
+{{ $Document->adresse['adress'] }}
+{{ $Document->adresse['zipcode'] }} {{ $Document->adresse['city'] }}
+{{ $Document->adresse['country'] }}
+{{ __('general_content.phone_trans_key') }} : {{ $Document->contact['number'] }}
+{{ __('general_content.email_trans_key') }} : {{ $Document->contact['mail'] }}
+<br />
+{{ __('general_content.identifier_trans_key') }}: {{ $Document->customer_reference }}
+                            </pre>
+                        @else
+<h1>{{ __('general_content.internal_order_trans_key') }}</h1>
                         @endif
                     </td>
-                    <td align="right" style="width: 40%;">
-                        <h3>{{ $Document->companie['label'] }}</h3>
-                        <pre>
-                            {{ $Document->contact['civility'] }} {{ $Document->contact['first_name'] }} {{ $Document->contact['name'] }}
-                            {{ $Document->adresse['adress'] }}
-                            {{ $Document->adresse['zipcode'] }} {{ $Document->adresse['city'] }}
-                            {{ $Document->adresse['country'] }}
-                            {{ __('general_content.phone_trans_key') }} : {{ $Document->contact['number'] }}
-                            {{ __('general_content.email_trans_key') }} : {{ $Document->contact['mail'] }}
-                        </pre>
-                    </td>
                 </tr>
-
             </table>
-        </div>
-        <br/>
-        <div class="invoice">
-            <div align="center"><h3>{{ $typeDocumentName}} - #{{ $Document->code }}</h3></div>
-            <hr style="color: #6c757d">
-            <table width="100%">
+        </header>
+
+        <footer>
+            <div>
+                <table width="100%">
+                    <tr>
+                        <td align="left" style="width: 40%;">
+                            &copy; {{ date('Y') }} - {{ $Factory->name }}
+                        </td>
+                        <td align="right" style="width: 40%;">
+                            <div class="pagenum-container">Page <span class="pagenum"></span></div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </footer>
+
+        <main>
+            <table style="width: 100%; " >
                 <thead>
                     <tr>
                         <th align="left">{{ __('general_content.order_trans_key') }}</th>
@@ -138,19 +157,6 @@
                     </td>
                 </tr>
             </table>
-        </div>
-
-        <div class="information" style="position: absolute; bottom: 0;">
-            <table width="100%">
-                <tr>
-                    <td align="left" style="width: 50%;">
-                        &copy; {{ date('Y') }} - {{ $Factory->name }}
-                    </td>
-                    <td align="right" style="width: 50%;">
-                        {{ $Factory->mail }}
-                    </td>
-                </tr>
-            </table>
-        </div>
+        </main>
     </body>
 </html>
