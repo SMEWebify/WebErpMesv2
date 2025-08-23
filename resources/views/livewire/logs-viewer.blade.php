9669dc9bcd72baf1c67d53e3162fd53175083d5d
Kevin <75578469+SMEWebify@users.noreply.github.com>
2025-08-23 16:08:48 +0200

    feat: add serial number activity history

--- resources/views/livewire/logs-viewer.blade.php
+++ resources/views/livewire/logs-viewer.blade.php
@@ -14,6 +14,16 @@
         </div>
         @endif
         <div class="form-group col-md-3">
+            <label for="user_id">{{ __('general_content.user_trans_key') }}</label>
+            <select class="form-control" id="user_id" wire:model.defer="userId">
+                <option value="">{{ __('general_content.view_all_trans_key') }}</option>
+                @foreach ($availableUsers as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+            @error('userId') <span class="text-danger">{{ $message }}<br/></span>@enderror
+        </div>
+        <div class="form-group col-md-3">
             <label for="date">{{__('general_content.start_date_trans_key') }}</label>
             <input type="date" class="form-control" id="start_date" wire:model.defer="startDate" placeholder="{{__('general_content.start_date_trans_key') }}">
             @error('startDate') <span class="text-danger">{{ $message }}<br/></span>@enderror
