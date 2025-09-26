87dd1c2f2a9fdbd6b255171dbaf0f8c038dcfc6f
Kevin <75578469+SMEWebify@users.noreply.github.com>
2025-09-26 16:34:45 +0200

    Remove hardcoded APP_KEY from tests

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -13,6 +13,13 @@ trait CreatesApplication
      */
     public function createApplication()
     {
+        if (empty($_ENV['APP_KEY']) && empty($_SERVER['APP_KEY']) && getenv('APP_KEY') === false) {
+            $key = 'base64:'.base64_encode(random_bytes(32));
+
+            $_ENV['APP_KEY'] = $_SERVER['APP_KEY'] = $key;
+            putenv('APP_KEY='.$key);
+        }
+
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
