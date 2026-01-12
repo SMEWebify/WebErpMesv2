3a7ab1c95d1d1c8f7c62c84bc87b3666ecd2fa23
billyboy35 <kevin.niglaut@wanadoo.fr>
2026-01-12 09:22:46 +0100

    Fix GHSA-pp68-5pc2-hv7w

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'auth:sanctum', 
         ],
     ];
 
