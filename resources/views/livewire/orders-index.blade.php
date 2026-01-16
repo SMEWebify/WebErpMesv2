c4ea5bf9e63b6e96cce4b46a0cb4a2b7cc188be6
Kevin <75578469+SMEWebify@users.noreply.github.com>
2026-01-16 21:48:57 +0100

    Add multi-status filter defaults for orders

--- resources/views/livewire/orders-index.blade.php
+++ resources/views/livewire/orders-index.blade.php
@@ -240,8 +240,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="fas fa-list"></i></span>
                             </div>
-                            <select class="form-control" name="searchIdStatus" id="searchIdStatus" wire:model.live="searchIdStatus">
-                                <option value="">{{ __('general_content.select_statu_trans_key') }}</option>
+                            <select class="form-control" name="searchIdStatus" id="searchIdStatus" wire:model.live="searchIdStatus" multiple>
                                 <option value="1">{{ __('general_content.open_trans_key') }}</option>
                                 <option value="2">{{ __('general_content.in_progress_trans_key') }}</option>
                                 <option value="3">{{ __('general_content.delivered_trans_key') }}</option>
@@ -250,6 +249,7 @@
                                 <option value="6">{{ __('general_content.canceled_trans_key') }}</option>
                             </select>
                         </div>
+                        <small class="form-text text-muted">{{ __('general_content.select_statu_trans_key') }}</small>
                     </div>
                 </div>
                 <div class="col-md-1">
